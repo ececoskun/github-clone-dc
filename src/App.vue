b7b98854bce3fd471706e9073b2a3d699a1bfbc2
ececoskun <ececk92@gmail.com>
2021-02-06 23:47:45 +0300

    navbar  page created

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-    <div id="app"></div>
+    <div id="app">
+        <nav/> 
+    </div>
 </template>
-
+<script> 
+import Nav from './components/Shared/Nav.vue'
+export default{
+  components: { Nav },
+    name: "App"
+}
+</script>
 <style lang="scss">
 </style>
